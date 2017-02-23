a80a36eefa9aaa2108c3250c1b45c0cb0aa3212f
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2017-02-23 19:58:23 +0100

    annotate example

--- DDP_Shiny_1.2/server.R
+++ DDP_Shiny_1.2/server.R
@@ -15,21 +15,23 @@ shinyServer(function(input, output) {
   # data input & presentation defined in ui.R (`sliderInput()` & `plotOutput()`)
   # [x] Are "output" & "input" reserved variable names (orange symbol in RStudio)?
   # ==> no, function parameters defined above
-  output$dots <- renderPlot({
+  output$dots <- renderPlot({ # reactivity requires parentheses with inner curly braces
     
     set.seed(2017-02-23)
     
+    # implement effects of inputs
     number_of_points <- input$numeric
-    
     minX <- input$sliderX[1]
     maxX <- input$sliderX[2]
     minY <- input$sliderY[1]
     maxY <- input$sliderY[2]
     
-    dataX <- runif(number_of_points, minX, maxX)
+    # generate random, uniform numbers
+    dataX <- runif(n = number_of_points, min = minX, max = maxX)
     dataY <- runif(number_of_points, minY, maxY)
     
-    xlab <- ifelse(input$show_xlab, "x axis", "")
+    # implement effects of checkboxes
+    xlab <- ifelse(test = input$show_xlab, yes = "x axis", no = "")
     ylab <- ifelse(input$show_ylab, "y axis", "")
     main <- ifelse(input$show_title, "title", "")
     
